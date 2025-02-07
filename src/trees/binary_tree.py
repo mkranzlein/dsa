2d613fe45a2b7ffbde31fcac73a32d11be78ab2e
Michael Kranzlein <8162250+mkranzlein@users.noreply.github.com>
2025-02-07 09:50:56 -0500

    Simplified insert with BFS

--- src/trees/binary_tree.py
+++ src/trees/binary_tree.py
@@ -1,3 +1,6 @@
+from collections import deque
+
+
 class Node:
     def __init__(self, val: int):
         self.val = val
@@ -5,20 +8,28 @@ class Node:
         self.right = None
 
 class NodeBinaryTree:
-    def __init__(self, root: Node = None):
+    def __init__(self, root: Node = None) -> None:
         self.root = root
 
-    def insert(self, val: int):
-        """Inserts a node into the next available position."""
-        if self.root is None:
-            self.root = Node(val)
-        node, insert_left = self.get_next_position()
-        if insert_left:
-            node.left = Node(val)
-        else:
-            node.right = Node(val)
+    def insert(self, val: int) -> None:
+        """Inserts into next available position with breadth-first search."""
+        queue = deque()
+        queue.append(self.root)
 
-    def insert_at(self, val: int, node: Node, insert_left: bool):
+        while len(queue) > 0:
+            node = queue.popleft()
+
+            if node.left is None:
+                node.left = Node(val)
+                return
+            if node.right is None:
+                node.right = Node(val)
+                return
+
+            queue.append(node.left)
+            queue.append(node.right)
+
+    def insert_at(self, val: int, node: Node, insert_left: bool) -> None:
         """Inserts a value as the left or right child of a specified node."""
         if insert_left:
             if node.left is not None:
@@ -28,22 +39,6 @@ class NodeBinaryTree:
             if node.right is not None:
                 raise RuntimeError("Position in tree is not available.")
             node.right = Node(val)
-
-    def get_next_position(self) -> tuple[Node, bool]:
-        """Returns next available position in tree.
-
-        Next available position is the left-most position of the bottom of the
-        tree or the left-most position on a new row.
-
-        Returns:
-            A tuple containing the parent node and a bool indicating whether
-            the next position is the node's left child (True)
-            or right child (False).
-        """
-        # What happens if no root?
-        # If deepest node is right edge of level, how do you know you're at
-        # right edge and need new level? Maybe traverse right to left?
-        raise NotImplementedError
 
     def get_deepest_node(self, node: Node, depth: int) -> tuple[Node, int]:
         if node is None:
@@ -57,9 +52,6 @@ class NodeBinaryTree:
             return (deepest_right, right_depth)
         else:
             return (deepest_left, left_depth)
-
-        # If just depth, could keep track of current depth or have external
-        # max_depth variable, but want to return node too.
 
     def delete(self, node: Node):
         raise NotImplementedError
